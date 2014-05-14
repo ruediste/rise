97d9f8786702785d1e6d95c9e9a763358799ffbd
Ruedi Steinmann <ruediste@gmail.com>
2014-05-14 07:24:23 +0200

    add testUtil project, fix project reorganization

--- testUtil/src/main/java/laf/MockitoExt.java
+++ testUtil/src/main/java/laf/MockitoExt.java
@@ -9,4 +9,9 @@ public class MockitoExt extends Mockito {
 	public static <T> T mock(TypeToken<T> type) {
 		return (T) Mockito.mock(type.getRawType());
 	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T mock(TypeToken<T> type, String name) {
+		return (T) Mockito.mock(type.getRawType(), name);
+	}
 }
