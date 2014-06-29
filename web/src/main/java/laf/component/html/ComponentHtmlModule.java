2b3f468374e2a7945c160c80c4d33705710da365
Ruedi Steinmann <ruediste@gmail.com>
2014-06-29 14:39:18 +0200

    fix pom

--- web/src/main/java/laf/component/html/ComponentHtmlModule.java
+++ web/src/main/java/laf/component/html/ComponentHtmlModule.java
@@ -1,6 +1,5 @@
 package laf.component.html;
 
-import laf.actionPath.ActionPathModule;
 import laf.base.BaseModule;
 import laf.component.core.ComponentCoreModule;
 import laf.http.HttpModule;
@@ -8,7 +7,7 @@ import laf.http.HttpModule;
 import org.jabsaw.Module;
 
 @Module(description = "HTML related functionality of the component framework", imported = { BaseModule.class }, exported = {
-		ActionPathModule.class, HttpModule.class, ComponentCoreModule.class })
+		HttpModule.class, ComponentCoreModule.class })
 public class ComponentHtmlModule {
 
 }
