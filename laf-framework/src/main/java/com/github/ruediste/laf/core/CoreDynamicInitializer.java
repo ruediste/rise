d4327cbd5c875eacd317b861387439dce65a4cd3
Ruedi Steinmann <ruediste@gmail.com>
2015-05-03 20:18:33 +0200

    add handler for ~reloadQuery

--- laf-framework/src/main/java/com/github/ruediste/laf/core/CoreDynamicInitializer.java
+++ laf-framework/src/main/java/com/github/ruediste/laf/core/CoreDynamicInitializer.java
@@ -1,7 +1,13 @@
 package com.github.ruediste.laf.core;
 
-import javax.inject.Inject;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
+
+import com.github.ruediste.laf.core.front.ReloadCountHolder;
+import com.github.ruediste.laf.core.httpRequest.HttpRequest;
 import com.github.ruediste.laf.core.web.assetPipeline.AssetPipelineConfiguration;
 import com.github.ruediste.laf.core.web.assetPipeline.AssetRequestMapper;
 import com.github.ruediste.laf.util.Initializer;
@@ -17,6 +23,15 @@ public class CoreDynamicInitializer implements Initializer {
 	@Inject
 	AssetRequestMapper assetRequestMapper;
 
+	@Inject
+	PathInfoIndex index;
+
+	@Inject
+	ReloadCountHolder holder;
+
+	@Inject
+	CoreRequestInfo info;
+
 	@Override
 	public void initialize() {
 		config.dynamicClassLoader = Thread.currentThread()
@@ -24,6 +39,34 @@ public class CoreDynamicInitializer implements Initializer {
 		config.initialize();
 		pipelineConfig.initialize();
 		assetRequestMapper.initialize();
+
+		index.registerPathInfo("/~reloadQuery", new RequestParser() {
+
+			@Override
+			public RequestParseResult parse(HttpRequest request) {
+
+				return new RequestParseResult() {
+
+					@Override
+					public void handle() {
+						boolean doReload = holder.waitForReload(Long
+								.parseLong(request.getParameter("nr")));
+						HttpServletResponse response = info
+								.getServletResponse();
+
+						response.setContentType("text/plain;charset=utf-8");
+						PrintWriter out;
+						try {
+							out = response.getWriter();
+							out.write(doReload ? "true" : "false");
+							out.close();
+						} catch (IOException e) {
+							throw new RuntimeException("Error sending response");
+						}
+					}
+				};
+			}
+		});
 	}
 
 }
