d3a94c98aa3251c7898538ac843f510e7c45ca1a
Ruedi Steinmann <ruediste@gmail.com>
2015-12-16 06:53:51 +0100

    cleanup Fileinput

--- framework/src/main/resources/com/github/ruediste/rise/core/web/fileinput/riseFileinput.js
+++ framework/src/main/resources/com/github/ruediste/rise/core/web/fileinput/riseFileinput.js
@@ -1,10 +1,3 @@
-(function() {
-	var reload=function(receiver){
-		receiver.find('input.rise_fileinput[type=file]').fileinput();
-	}
-	// perform on reloads
-	rise.onReload.add(reload);
-	
-	// initialize all file inputs
-	$(function(){reload($(document));});
-})();
+rise.onReload.add(function(receiver) {
+	receiver.find('input.rise_fileinput[type=file]').fileinput();
+});
