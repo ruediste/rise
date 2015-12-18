765941abe8df8acb83cefefaadd1798fbb4dd04f
Ruedi Steinmann <ruediste@gmail.com>
2015-12-18 16:57:03 +0100

    remove file upload from sample app

--- sample-app/src/main/java/com/github/ruediste/rise/sample/welcome/WelcomeView.java
+++ sample-app/src/main/java/com/github/ruediste/rise/sample/welcome/WelcomeView.java
@@ -5,7 +5,6 @@ import com.github.ruediste.rise.sample.component.SampleComponentController;
 import com.github.ruediste.rise.sample.crud.CrudController;
 import com.github.ruediste.rise.sample.db.PageView;
 import com.github.ruediste.rise.sample.db.TodoController;
-import com.github.ruediste.rise.sample.fileupload.FileUploadController;
 import com.github.ruediste1.i18n.label.Label;
 import com.github.ruediste1.i18n.label.Labeled;
 
@@ -20,7 +19,6 @@ public class WelcomeView
         html.bButtonA().HREF(go(TodoController.class).index())
                 .content("Todo Items");
         html.bButtonA().HREF(go().error()).content("Page with error");
-        html.rButtonA(go(FileUploadController.class).index());
         html.rButtonA(go(CrudController.class).showTodos());
         html.rButtonA(go(CrudController.class).showTodoCategories());
         html.rButtonA(go(SampleComponentController.class).index());
